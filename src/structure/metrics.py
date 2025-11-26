41e8b11e9e513b4482550aceda577fb00cc79635
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2025-11-26 11:07:15 -0600

    remove duplicate secondary structure

--- src/structure/metrics.py
+++ src/structure/metrics.py
@@ -11,26 +11,6 @@ from src.sequence import utils
 
 
 # TODO: move helper functions to separate file so only @registered_metric functions remain here
-@register_metric(name='define_secondary_structure',
-                 provides=['ss_group', 'ss_domains'],
-                 tags={'structure'})
-def calculate_secondary_structure(array: struc.AtomArray) -> np.ndarray:
-    """
-    Calculate secondary structure assignment per residue.
-
-    Parameters:
-    -----------
-    array : AtomArray
-        Structure array (amino acids only recommended)
-
-    Returns:
-    --------
-    np.ndarray
-        Per-residue secondary structure assignment.
-        'a' = alpha-helix, 'b' = beta-sheet, 'c' = coil
-    """
-    return struc.annotate_sse(array)
-
 
 @register_metric(name='sasa', provides=['sasa'], tags={'structure'})
 def calculate_sasa(array: struc.AtomArray, vdw_radii: str = "ProtOr") -> pd.DataFrame:
@@ -131,7 +111,6 @@ def calculate_membrane_distance(array: struc.AtomArray) -> pd.DataFrame:
     metadata_df['distance_from_membrane_edge'] = distance_from_edge
 
     return metadata_df
-
 
 def define_secondary_structure(array: struc.AtomArray) -> pd.DataFrame:
     """Calculate secondary structure and merge adjacent regions based on heuristics or membrane information"""
